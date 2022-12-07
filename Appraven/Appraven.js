563620ac937a604f46dd1cf4965a226c674f8a59
Dell <dvq273665536@gmail.com>
2022-12-07 13:40:58 +0700

    update

--- Appraven/Appraven.js
+++ Appraven/Appraven.js
@@ -10,22 +10,13 @@ const tmp2 = '/AppRaven/social';
 const tmp3 = '/AppRaven/buy';
 
 if (url.indexOf(tmp1) != -1) {
-	var body = $response.body.replace(/premium": false/g, 'premium": true');
+ var body = $response.body.replace(/premium": false/g, 'premium": true');
 }
 if (url.indexOf(tmp2) != -1) {
-	var body = $response.body.replace(/premium": false/g, 'premium": true');
+ var body = $response.body.replace(/premium": false/g, 'premium": true');
 }
 if (url.indexOf(tmp3) != -1) {
-	obj={"success":true,"message":"1896165181","isReceiptValid":true,"isSubscriptionActive":true};
-	body = JSON.stringify(obj);
+ obj={"success":true,"message":"1896165181","isReceiptValid":true,"isSubscriptionActive":true};
+ body = JSON.stringify(obj);
 }
-$done({body});
-obj.entitlement.storage={
-      "used": 0,
-      "limit": 1154487209165,
-      "display_limit": 1099511627776,
-      "warn": 992137445376
-    };
-obj.avatar.placeholder=true;
-body = JSON.stringify(obj); 
 $done({body});
