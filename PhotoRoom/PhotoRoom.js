13c4dfa38d45a5e0ea0650f5ef53276e910575fa
Dell <dvq273665536@gmail.com>
2022-12-08 12:08:02 +0700

    update

---
+++ PhotoRoom/PhotoRoom.js
@@ -0,0 +1,10 @@
+/*
+Chức năng kịch bản：Unlock PhotoRoom
+Phản hồi：https://t.me/MynameisDell
+*/
+
+var obj = JSON.parse($response.body); 
+obj['is_valid'] = true;
+obj['is_trial'] = true;
+obj['expiration_date'] = '2970-01-01T00:00:00Z';
+$done({body: JSON.stringify(obj)});
