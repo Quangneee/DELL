a0306f8453ffc82bc65679b9adc011e519bfdb39
Dell <dvq273665536@gmail.com>
2022-12-09 11:44:18 +0700

    update

---
+++ Xmind- Mind Map & Brainstorm/Xmind- Mind Map & Brainstorm.js
@@ -0,0 +1,6 @@
+/*
+Chức năng kịch bản：Unlock Xmind- Mind Map & Brainstorm
+Phản hồi：https://t.me/MynameisDell
+*/
+
+var _0x49754b=JSON['parse']($response['body']);_0x49754b['license']['status']='sub';_0x49754b['license']['expireTime']=0x1b8d90e4000;$done({'body':JSON['stringify'](_0x49754b)});
